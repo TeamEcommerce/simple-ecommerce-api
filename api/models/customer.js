a044f612dc55fa97b644f7ae2669e0f944b7d892
Willy Aguirre <marti1125@gmail.com>
2017-08-22 00:04:06 -0500

    add customer model

---
+++ api/models/customer.js
@@ -0,0 +1,19 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var CustomerSchema = new Schema({
+	first_name: { type: String },
+  last_name: { type: String },
+  email: { type: String, unique: true },
+  password: { type: String },
+  phome: { type: String },
+  address: { type: String },
+  city: { type: String },
+  country: { type: String },
+  active: { type: Boolean, default: true },
+  created_date: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Customer', CustomerSchema);
